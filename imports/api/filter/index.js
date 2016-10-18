0dc98aaa34b1d468d9e4360537c3327a8a3945b0
objectsforheads <dev@objectsforheads.com>
2016-10-17 18:14:54 -0700

    fix a bug where the database _id was unintentionally being delivered along with the card info

--- imports/api/filter/index.js
+++ imports/api/filter/index.js
@@ -1,7 +1,7 @@
 Meteor.methods({
   filterCards: function(filters) {
     check(filters, Object);
-    filteredCards = CardsCurrent.find(filters).fetch();
+    filteredCards = CardsCurrent.find(filters, {fields: {_id: 0}}).fetch();
     return filteredCards;
   }
 });
