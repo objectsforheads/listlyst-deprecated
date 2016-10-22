89bf765d616b28253db341e407dfc4827893ec00
objectsforheads <dev@objectsforheads.com>
2016-10-22 11:53:25 -0700

    allow google-analyics origin in browser policy

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,6 +2,9 @@
 BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
 BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
 
+// Allow Google Analytics
+BrowserPolicy.content.allowOriginForAll("www.google-analytics.com");
+
 // Allow CDNs
 BrowserPolicy.content.allowOriginForAll('cdnjs.cloudflare.com');
 
